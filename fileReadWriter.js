50e5e068132beb1400378c0aeecd485465abd323
Colin Campbell <colin.5.campbell@bt.com>
2016-06-30 15:40:10 +0100

    File read/write functions in webService.js spilt into new fileReadWriter.js

---
+++ fileReadWriter.js
@@ -0,0 +1,127 @@
+var fs = require('fs');
+var builder = require('xmlbuilder');
+var parseXMLstring = require('xml2js').parseString;
+
+module.exports = {
+	products:  [null],
+	lists: [null],
+
+	GetProductListFromFile: function (res){
+	    fs.readFile('src/txt/ProductList.txt', 'utf8', function (err,data) {
+		  	if (err) {
+		    	return console.log(err);
+		  	}
+		  	var items = "";
+		  	items = data;
+		  	products = items.split("\n");
+		  	res.send(products);
+			res.end();
+		});
+	},
+
+	ReadListsFromFiles: function (res){
+		fs.readdir('src/txt/lists', 'utf8', function (err, files) {
+			if (err) {
+		    	return console.log(err);
+		  	}
+		  	this.lists = [];
+			for(i = 0; i < files.length; i++){
+				var name = files[i].substring(0,files[i].indexOf('.txt'))
+				if(this.lists === undefined){
+					this.lists = [name];
+				}else{
+					this.lists.push(name);
+				}
+			}
+			res.send(lists);
+			res.end();
+		});
+	},
+
+	WriteListsToFiles: function (lists){
+		for(var i = 0; i < lists.length; i++){
+			WriteListToFile(lists[i]);
+		}
+		fs.readdir('src/txt/lists', 'utf8', function(err, files) {
+			if (err) {
+		    	return console.log(err);
+		  	}
+			console.log('List of files: ' + files);
+			for(i = 0; i < files.length; i++){
+				for(var ii = 0; ii < lists.length; ii++){
+					if(files[i] === (lists[ii].name + ".txt")){
+						files.splice(i,1);
+						i--;
+					}
+				}
+				
+			}
+			for(iii = 0; iii < files.length; iii++){
+				 fs.unlink('src/txt/lists/' + files[iii], function(err){
+			        if (err) {
+				    	return console.log(err);
+				  	}
+	          });
+			}
+		});
+	},
+
+	RemoveListFromFile: function (list){
+		var name = list + ".txt";
+		fs.unlink('src/txt/lists/' + name, function(err){
+			if (err) {
+				return console.log(err);
+			}
+	    });
+	},
+
+	ReadListFromFile: function (list, res){
+		var name =  list + '.txt';
+		fs.readFile('src/txt/lists/' + name, 'utf8', function (err,data) {
+		  	if (err) {
+		    	return console.log(err);
+		  	}
+		  	console.log("read file test:" + data);
+		  	parseXMLstring(data, function (err, result) {
+			    //console.log(result);
+			    var list = { name: result.list.name[0], items: []};
+			    if(result.list.items[0].item != undefined){
+			    for(var i = 0; i < result.list.items[0].item.length; i++){
+				    	var item = { name: result.list.items[0].item[i].name[0], quantity: result.list.items[0].item[i].quantity[0]};
+						if(list.items === undefined){
+							list.items = [item];
+				  		}else{
+				  			list.items.push(item);
+				  		}
+				  	}
+				}
+			  	console.log(list);
+			    res.send(list);
+			    res.end();
+			});
+		});
+	},
+
+	WriteListToFile: function (list){
+		var name = list.name + ".txt";
+
+		var xml = builder.create('list');
+			xml.ele('name', list.name);
+			var items = xml.ele('items');
+				for(i = 0; i < list.items.length; i++){
+					var item = items.ele('item');
+					item.ele('name', list.items[i].name);
+					item.ele('quantity', list.items[i].quantity);
+				}
+			xml.end({ pretty: true});
+			 
+			//console.log(xml);
+
+		fs.writeFile('src/txt/lists/' + name, xml, 'utf8', function(err) {
+			if (err) {
+		    	return console.log(err);
+		  	}
+			console.log('It\'s saved!');
+		});
+	}
+}
--- webService.js
+++ webService.js
@@ -1,144 +1,13 @@
 var express = require('express');
 var webService = express();
-var fs = require('fs');
-var builder = require('xmlbuilder');
-var parseXMLstring = require('xml2js').parseString;
 var bodyParser = require('body-parser');
+var file = require('./fileReadWriter.js');
 
 webService.use(bodyParser.json()); // for parsing application/json
 webService.use(bodyParser.urlencoded({ extended: true })); // for parsing
 
 var webServiceUrl = 'http://localhost:';
 var webServicePort = '3001';
-
-var products = [];
-GetProductListFromFile();
-var lists = [];
-//GetListsFromFile();
-ReadListsFromFiles();
-
-//list object
-function listObj(name, items){
-	this.name = name;
-	this.items = items;
-}
-
-//item object
-function itemObj(name, quantity){
-	this.name = name;
-	this.quantity = quantity;
-}
-
-function GetProductListFromFile(){
-    fs.readFile('src/txt/ProductList.txt', 'utf8', function (err,data) {
-	  	if (err) {
-	    	return console.log(err);
-	  	}
-	  	var items = "";
-	  	items = data;
-	  	products = items.split("\n");
-	});
-	
-};
-
-function ReadListsFromFiles(){
-	fs.readdir('src/txt/lists', 'utf8', function (err, files) {
-		if (err) {
-	    	return console.log(err);
-	  	}
-		for(i = 0; i < files.length; i++){
-			var name = files[i].substring(0,files[i].indexOf('.txt'))
-			lists.push(name);
-		}
-	});
-}
-
-function WriteListsToFiles(lists){
-	for(var i = 0; i < lists.length; i++){
-		WriteListToFile(lists[i]);
-	}
-	fs.readdir('src/txt/lists', 'utf8', function(err, files) {
-		if (err) {
-	    	return console.log(err);
-	  	}
-		console.log('List of files: ' + files);
-		for(i = 0; i < files.length; i++){
-			for(var ii = 0; ii < lists.length; ii++){
-				if(files[i] === (lists[ii].name + ".txt")){
-					files.splice(i,1);
-					i--;
-				}
-			}
-			
-		}
-		for(iii = 0; iii < files.length; iii++){
-			 fs.unlink('src/txt/lists/' + files[iii], function(err){
-		        if (err) {
-			    	return console.log(err);
-			  	}
-          });
-		}
-	});
-}
-
-function RemoveListFromFile(list){
-	var name = list + ".txt";
-	fs.unlink('src/txt/lists/' + name, function(err){
-		if (err) {
-			return console.log(err);
-		}
-    });
-}
-
-function ReadListFromFile(list, res){
-	var name =  list + '.txt';
-	fs.readFile('src/txt/lists/' + name, 'utf8', function (err,data) {
-	  	if (err) {
-	    	return console.log(err);
-	  	}
-	  	//console.log("read file test:" + data);
-	  	parseXMLstring(data, function (err, result) {
-		    //console.log(result);
-		    var list = new listObj(result.list.name[0],[]);
-		    if(result.list.items[0].item != undefined){
-		    for(var i = 0; i < result.list.items[0].item.length; i++){
-			    	var item = new itemObj(result.list.items[0].item[i].name[0], result.list.items[0].item[i].quantity[0]);
-					if(list.items === undefined){
-						list.items = [item];
-			  		}else{
-			  			list.items.push(item);
-			  		}
-			  	}
-			}
-		  	console.log(list);
-		    res.send(list);
-		    res.end();
-		});
-	});
-}
-
-function WriteListToFile(list){
-	var name = list.name + ".txt";
-
-	var xml = builder.create('list');
-		xml.ele('name', list.name);
-		var items = xml.ele('items');
-			for(i = 0; i < list.items.length; i++){
-				var item = items.ele('item');
-				item.ele('name', list.items[i].name);
-				item.ele('quantity', list.items[i].quantity);
-			}
-		xml.end({ pretty: true});
-		 
-		//console.log(xml);
-
-	fs.writeFile('src/txt/lists/' + name, xml, 'utf8', function(err) {
-		if (err) {
-	    	return console.log(err);
-	  	}
-		console.log('It\'s saved!');
-	});
-}
 
 webService.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -148,14 +17,12 @@ webService.use(function(req, res, next) {
 
 webService.get('/getProductList', function(req, res, next){
 	console.log('getProductList');
-	res.send(products);
-	res.end();
+	file.GetProductListFromFile(res);
 });
 
 webService.get('/getLists', function(req, res , next){
 	console.log('getLists');
-	res.send(lists);
-	res.end();
+	file.ReadListsFromFiles(res);
 });
 
 webService.post('/getList', function(req, res, next){
@@ -165,12 +32,8 @@ webService.post('/getList', function(req, res, next){
 	//var listNew = 'List1';
 	console.log('get List');
 	console.log(listNew);
-	for(i = 0; i < lists.length; i++){
-		if(lists[i] == listNew){
-			//console.log(ReadListFromFile(lists[i]));
-			ReadListFromFile(lists[i], res);
-			break;
-		}
+	if(listNew != undefined){
+		file.ReadListFromFile(listNew, res);
 	}
 });
 
@@ -178,12 +41,12 @@ webService.post('/sendList', function(req, res, next) {
     var listNew = req.body;
 	console.log('sendList');
 	console.log(listNew);
-	WriteListToFile(listNew)
-	var i = lists.indexOf(listNew.name);
+	file.WriteListToFile(listNew)
+	var i = file.lists.indexOf(listNew.name);
 	if(i == -1){
-		lists.push(listNew.name);
+		file.lists.push(listNew.name);
 	}else{
-		lists[i] = listNew.name;
+		file.lists[i] = listNew.name;
 	}
     res.end();
 });
@@ -192,8 +55,8 @@ webService.post('/sendListRemove', function(req, res, next) {
     var listRemove = req.body.list;
 	console.log('sendListRemove');
 	console.log(listRemove);
-	RemoveListFromFile(listRemove)
-	lists.splice(lists.indexOf(listRemove),1);
+	file.RemoveListFromFile(listRemove)
+	file.lists.splice(file.lists.indexOf(listRemove),1);
     res.end();
 });
 
